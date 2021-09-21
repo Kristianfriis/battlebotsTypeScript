cd9050b624a7820d9d297e90a565ba85a15b77e1
Kristian Friis <friis1212@gmail.com>
2021-09-21 15:39:09 +0200

    added attack array to client

--- backend/src/models/Client.ts
+++ backend/src/models/Client.ts
@@ -1,11 +1,17 @@
 import { Response } from "express";
+import { Attack } from "./Attack";
 
 export class Client{
     BattleId : number;
     Response : Response;
+    Attacks : Attack[] = []
 
     constructor(battleId : number, response : Response){
         this.BattleId = battleId;
         this.Response = response
+    }
+
+    AddAttack(att : Attack){
+        this.Attacks.push(att)
     }
 }
